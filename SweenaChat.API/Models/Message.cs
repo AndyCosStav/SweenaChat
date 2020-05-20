49a0cb15188f6d8863fcc687748c3380d213ffde
Andy <andystav91@gmail.com>
2020-05-20 17:38:13 +0100

    Add project files.

---
+++ SweenaChat.API/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweenaChat.API.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        public string MessageContent { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public User Sender { get; set; }
+
+        public User Receiver { get; set; }
+    }
+}
---
+++ SweenaChat.API/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweenaChat.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string LoginId { get; set; }
+
+        public string Username { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public List <Message> Messages { get; set; }
+
+    }
+}
