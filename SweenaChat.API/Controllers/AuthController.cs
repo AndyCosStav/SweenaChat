d26c46462c20e19a7cbd55e8a33571717889ffbe
Andy <andystav91@gmail.com>
2020-05-28 19:53:29 +0100

    adding jwt functionality and login and reset password endpoints

--- SweenaChat.API/Controllers/AuthController.cs
+++ SweenaChat.API/Controllers/AuthController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using SweenaChat.API.Data;
 using SweenaChat.API.Models;
 using SweenaChat.API.ViewModels;
@@ -23,6 +27,60 @@ namespace SweenaChat.API.Controllers
             _userManager = userManager;
             _configuration = configuration;
             _dbContext = dbContext;
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public async Task<ActionResult> Login([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var claim = new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName)
+                };
+                var signinKey = new SymmetricSecurityKey(
+                  Encoding.UTF8.GetBytes(_configuration["Jwt:SigningKey"]));
+
+                int expiryInMinutes = Convert.ToInt32(_configuration["Jwt:ExpiryInMinutes"]);
+
+                var token = new JwtSecurityToken(
+                  issuer: _configuration["Jwt:Site"],
+                  audience: _configuration["Jwt:Site"],
+                  expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                  signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+
+                return Ok(
+                  new
+                  {
+                      token = new JwtSecurityTokenHandler().WriteToken(token),
+                      expiration = token.ValidTo
+                  });
+            }
+            return Unauthorized("did not log in");
+        }
+
+
+
+        [Route("ResetPassword")]
+        [HttpPost]
+        public async Task<ActionResult> ResetPassword([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var result = await _userManager.ChangePasswordAsync(user, model.Password, model.NewPassword);
+
+                if (result.Succeeded)
+                {
+                    return Ok(new { Username = user.UserName });
+                }
+
+            }
+
+            return Unauthorized("password not changed");
         }
 
         [Route("register")]
